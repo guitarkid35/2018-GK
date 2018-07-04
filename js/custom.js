8905367c264f6f291e9f9dd261eadfb33bcb4a77
GK <guitarkid35@gmail.com>
2018-07-04 20:10:38 +0800

    gk

---
+++ js/custom.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	//header color change
+	$(window).on("scroll",function(){
+		var scrollDis = $(window).scrollTop();
+		var $header = $(".js-header");
+		if(scrollDis > 80){
+			$header.addClass("header--colorchange");
+		}
+		else{
+			$header.removeClass("header--colorchange");
+		}
+	})
+	//WOW-plugin
+	if(screen.width > 768){
+		new WOW().init();
+	}
+
+	//stellar
+	if(screen.width > 768){
+		$(window).stellar();
+	}
+	//slide
+	$('#at').click(function(){
+		$('html,body').animate({scrollTop:$('#about').offset().top}, 900, 'easeInOutCirc');
+	});
+	$('#sk').click(function(){
+		$('html,body').animate({scrollTop:$('#skills').offset().top}, 900, 'easeInOutCirc');
+	});
+	$('#ex').click(function(){
+		$('html,body').animate({scrollTop:$('#experience').offset().top}, 900, 'easeInOutCirc');
+	});
+	$('#wk').click(function(){
+		$('html,body').animate({scrollTop:$('#works').offset().top}, 900, 'easeInOutCirc');
+	});
+	$('#ct').click(function(){
+		$('html,body').animate({scrollTop:$('#contact').offset().top}, 900, 'easeInOutCirc');
+	});
+	$('#tk').click(function(){
+		$('html,body').animate({scrollTop:$('#contact').offset().top}, 900, 'easeInOutCirc');
+	});
+})
+
